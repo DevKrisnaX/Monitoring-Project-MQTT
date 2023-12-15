3060acc44fa71a216e1f35949f710e3f523d18a3
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2023-12-15 17:05:50 +0700

    FIX POSITION

--- Monitoring.py
+++ Monitoring.py
@@ -1,5 +1,6 @@
 # Library yang digunakan
 from tkinter import *
+import os
 import random
 import time
 import datetime
@@ -9,7 +10,7 @@ import sqlite3
 
 # Setting Server
 broker = 'tr3.localto.net'
-port = 36733
+port = 32803
 topic = "Jamur"
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 
@@ -21,7 +22,9 @@ cur = con.cursor()
 buat_tabel = '''CREATE TABLE IF NOT EXISTS JamurTemp(
                     time TEXT NOT NULL,
                     rak1_temp TEXT NOT NULL,
-                    rak1_hum TEXT NOT NULL
+                    rak1_hum TEXT NOT NULL,
+                    rak2_temp TEXT NOT NULL,
+                    rak2_hum TEXT NOT NULL
                    
                 );'''
 
@@ -67,28 +70,30 @@ def subscribe(client: mqtt_client):
             spray2 = str(_data["spray2"])
 
             print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-            data_sensor_val = (current_time, temp1, moist1)
-            cur.execute("INSERT INTO JamurTemp (time,rak1_temp,rak1_hum) VALUES(?, ?, ?)",(data_sensor_val))
+            data_sensor_val = (current_time, temp1, moist1, temp2, moist2)
+            cur.execute("INSERT INTO JamurTemp (time,rak1_temp,rak1_hum,rak2_temp,rak2_hum) VALUES(?, ?, ?, ?, ?)",(data_sensor_val))
             con.commit()
 
             #RAK1
-            temp_label1.place(x=155, y=217, anchor=CENTER)
+            temp_label1.place(x=315, y=457, anchor=CENTER)
             temp_label1.config(text=temp1)
-            moist_label1.place(x=155, y=327, anchor=CENTER)
+            moist_label1.place(x=315, y=576, anchor=CENTER)
             moist_label1.config(text=moist1)
-            Kipas_label1.place(x=160, y=447, anchor=CENTER)
-            Kipas_label1.config(text=kipas1)
-            Spray_label1.place(x=160, y=553, anchor=CENTER)
+            Spray_label1.place(x=396, y=288, anchor=CENTER)
             Spray_label1.config(text=spray1)
+            Kipas_label1.place(x=396, y=363, anchor=CENTER)
+            Kipas_label1.config(text=kipas1)
+
             #RAK2
-            temp_label2.place(x=500, y=217, anchor=CENTER)
+            temp_label2.place(x=750, y=457, anchor=CENTER)
             temp_label2.config(text=temp2)
-            moist_label2.place(x=500, y=327, anchor=CENTER)
+            moist_label2.place(x=750, y=575, anchor=CENTER)
             moist_label2.config(text=moist2)
-            Kipas_label2.place(x=500, y=447, anchor=CENTER)
-            Kipas_label2.config(text=kipas2)
-            Spray_label2.place(x=500, y=553, anchor=CENTER)
+            Spray_label2.place(x=833, y=288, anchor=CENTER)
             Spray_label2.config(text=spray2)
+            Kipas_label2.place(x=833, y=363, anchor=CENTER)
+            Kipas_label2.config(text=kipas2)
+
 
             time.sleep(1)
         except json.decoder.JSONDecodeError as e:
@@ -102,39 +107,39 @@ def subscribe(client: mqtt_client):
 
 window = Tk()
 window.title("MONITORING JAMUR DASHBOARD")
-window.geometry('1440x1024')  # Width, Height
+window.geometry('1000x700')  # Width, Height
 window.resizable(False, False)
 window.configure(bg="white")
 
 # Banner image
-canvas = Canvas(window, width=1440, height=1024)
+canvas = Canvas(window, width=1000, height=700)
 canvas.place(x=0, y=0)
-img = PhotoImage(file="image\Layout.jpg")
+img = PhotoImage(file="image\Layout.png")
 canvas.create_image(0, 0, anchor=NW, image=img)
 
 #### RAK 1
 # Label °C dan % 1
-tempC_label1 = Label(window, text=" °C", bg="white", fg="black", font=("Horta", 20))
-tempC_label1.place(x=170,y=200)
-moistP_label1 = Label(window, text=" %", bg="white", fg="black", font=("Horta", 20))
-moistP_label1.place(x=175,y=310)
+tempC_label1 = Label(window, text=" °C", bg="white", fg="black", font=("Horta", 15))
+tempC_label1.place(x=329, y=443)
+moistP_label1 = Label(window, text=" %", bg="white", fg="black", font=("Horta", 15))
+moistP_label1.place(x=331, y=562)
 
 temp_label1 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
 moist_label1 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
-Kipas_label1 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
-Spray_label1 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
+Kipas_label1 = Label(window,text="",bg="white",fg="black",font=("Horta", 15))
+Spray_label1 = Label(window,text="",bg="white",fg="black",font=("Horta", 15))
 
 #### RAK 2
 # Label °C dan % 2
-tempC_label2 = Label(window, text=" °C", bg="white", fg="black", font=("Horta", 20))
-tempC_label2.place(x=510,y=200)
-moistP_label2 = Label(window, text=" %", bg="white", fg="black", font=("Horta", 20))
-moistP_label2.place(x=515,y=310)
+tempC_label2 = Label(window, text=" °C", bg="white", fg="black", font=("Horta", 15))
+tempC_label2.place(x=770,y=443)
+moistP_label2 = Label(window, text=" %", bg="white", fg="black", font=("Horta", 15))
+moistP_label2.place(x=770,y=562)
 
 temp_label2 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
 moist_label2 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
-Kipas_label2 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
-Spray_label2 = Label(window,text="",bg="white",fg="black",font=("Horta", 20))
+Kipas_label2 = Label(window,text="",bg="white",fg="black",font=("Horta", 15))
+Spray_label2 = Label(window,text="",bg="white",fg="black",font=("Horta", 15))
 
 
 def run():
